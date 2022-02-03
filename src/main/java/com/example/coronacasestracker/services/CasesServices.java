db2a608b399e54eed37df903ae5a8194d6c07b6e
Updesh <updesh7457@gmail.com>
2022-02-03 18:16:41 +0530

    add concept to display only new cases

--- src/main/java/com/example/coronacasestracker/services/CasesServices.java
+++ src/main/java/com/example/coronacasestracker/services/CasesServices.java
@@ -13,13 +13,28 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class CasesServices {
 
     private static String Cases_DATA_URL = "https://covid19.who.int/WHO-COVID-19-global-data.csv";
+
+    Date currentDate = new Date();
+
+    private static final long ONE_DAY_MILLI_SECONDS = 24 * 60 * 60 * 1000;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    long previousDayMilliSeconds = currentDate.getTime() - ONE_DAY_MILLI_SECONDS;
+    Date previousDate = new Date(previousDayMilliSeconds);
+    String prvsDate = dateFormat.format(previousDate);
+
+
 
     private List<CoronaStats> allStats = new ArrayList<>();
 
@@ -40,10 +55,12 @@ public class CasesServices {
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
 
-            //if (java.time.LocalDate.now().toString() == record.get("Date_reported")) {
 
 
 
+
+
+            if (prvsDate.equals(record.get(0))) {
 
                 CoronaStats coronaStats = new CoronaStats();
                 coronaStats.setCountry(record.get("Country"));
@@ -55,7 +72,7 @@ public class CasesServices {
                 coronaStats.setDiffFromPrevDay(latestCases - prevDayCases);
                 newStats.add(coronaStats);
             }
-       // }
+       }
         this.allStats = newStats;
     }
 }
